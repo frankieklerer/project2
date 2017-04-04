fdd8380d38db924558a921eb39033727fe5e9da2
frankieklerer <frankie@klerer.net>
2017-04-04 10:09:38 -0400

    plz dont mess anything up

--- ReceiverApplication.java
+++ ReceiverApplication.java
@@ -5,7 +5,7 @@
 public class ReceiverApplication
 {
     public void receiveMessage(Message msg){
-        System.out.println("from receiver:" + msg.getMessage());
+       // System.out.println("from receiver:" + msg.getMessage());
     }
 
 }
